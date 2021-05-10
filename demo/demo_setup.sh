226b4e955eedb7493025b9f60a7a227c2a4aec14
Snehal Jauhri <snehal.jauhri@gmail.com>
2021-05-10 17:42:00 +0200

    fixed demo

--- demo/demo_setup.sh
+++ demo/demo_setup.sh
@@ -8,6 +8,6 @@ source devel/setup.bash
 source /PX4/Firmware/Tools/setup_gazebo.bash /PX4/Firmware /PX4/Firmware/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/PX4/Firmware:/PX4/Firmware/Tools/sitl_gazebo
 
-# Launch drone and rover in fire_station simulation
-roslaunch fire_station_description bringup_uav_rover.launch
+# Launch drone in fire_station simulation
+roslaunch fire_station_description bringup_uav.launch
 
--- demo/launch_pos_control.sh
+++ demo/launch_pos_control.sh
@@ -4,4 +4,4 @@
 source /projects/ros_packages/devel/setup.bash
 
 # Start pos_control script
-rosrun fire_station_pos_control FLYinCollapsedFireStation_rover.py
+rosrun fire_station_pos_control FLYinCollapsedFireStation.py
