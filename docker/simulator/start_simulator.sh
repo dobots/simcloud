2a6aec019a97df73f8147cb8fa909b00f5fbcc8b
Ubuntu <ubuntu@ip-172-31-39-124.eu-west-3.compute.internal>
2021-06-30 15:09:13 +0000

    added automated startup of webterminal into the simulator Docker image

--- docker/simulator/start_simulator.sh
+++ docker/simulator/start_simulator.sh
@@ -1,2 +1,10 @@
-export user_id=ole sim_no=one
-docker-compose up -d
+#!/bin/bash
+
+export user_id="reka" sim_no="one"
+docker-compose -p _{$user_id}_{$sim_no} up -d
+
+#start of the web terminal
+docker exec -it -d $user_id"_"$sim_no"_simulator_1" ./web_terminal_scripts/start_web_terminal.sh
+
+#start of gzweb
+#docker exec -it -d $user_id"_"$sim_no"_simulator_1" cd /gzweb && npm start
