f69f8aa1d15c573af718e83c1fff8d434ffb718e
b00tss <bart_bootsma@hotmail.com>
2022-02-02 10:16:48 +0100

    added project_ws workspace for personal projects

---
+++ projects_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/projects/projects_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/our_ros_packages/devel;/PX4/devel;/opt/ros/melodic'.split(';') if '/our_ros_packages/devel;/PX4/devel;/opt/ros/melodic' != '' else []
---
+++ projects_ws/build/factory_hall_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /projects/projects_ws/src/factory_hall_description
+# Build directory: /projects/projects_ws/build/factory_hall_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
